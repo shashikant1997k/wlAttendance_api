5c4d21bbc00b5b8ab386076ac055e51ba1ada9bd
shashikant <shashikant.k@wastelink.co>
2021-06-08 13:32:39 +0530

    userlogin

--- user/views.py
+++ user/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect
 from rest_framework import generics
 from rest_framework import mixins
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import User
 from .serializers import UserSerializer, UserLoginSerializer, UserLogoutSerializer
@@ -34,7 +34,7 @@ class Login(generics.GenericAPIView):
         serializer_class = UserLoginSerializer(data=request.data)
         if serializer_class.is_valid(raise_exception=True):
             return Response(serializer_class.data, status=HTTP_200_OK)
-        return Response(serializer_class.errors, status=HTTP_400_BAD_REQUEST)
+        return Response(serializer_class.errors, status=HTTP_401_UNAUTHORIZED)
 
 
 class Logout(generics.GenericAPIView):
